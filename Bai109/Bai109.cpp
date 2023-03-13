5bd3b95438ca7ca25befbcd9ec3e5d2616dd66ee
TTMTN <127732884+TTMTN@users.noreply.github.com>
2023-03-14 00:01:48 +0700

    Add Bai109

---
+++ Bai109/Bai109.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float s = 1;
+	float e = 1;
+	int m = 1;
+	int i = 1;
+	while (e >= pow(10, -6))
+	{
+		m = m * i;
+		e = (float)1 / m;
+		s = s + e;
+		i = i + 1;
+	}
+	cout << "e" << " = " << s;
+	return 0;
+}
