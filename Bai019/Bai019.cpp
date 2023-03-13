9c21c9f917d7aa5a5d8927586f636095e89bfa6b
TTMTN <127732884+TTMTN@users.noreply.github.com>
2023-03-13 23:39:29 +0700

    Add Bai019

---
+++ Bai019/Bai019.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "Nhap x= ";
+	cin >> x;
+	int x2 = x * x;
+	int x4 = x2 * x2;
+	int x8 = x4 * x4;
+	int x12 = x8 * x4;
+	int x13 = x12 * x;
+	cout << " x13= " << x13;
+	return 0;
+}
